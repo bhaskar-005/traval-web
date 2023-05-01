c9071029c7876a90b9341873f45a62e20a095bc3
Bhaskar bhandari <122281535+bhasker440@users.noreply.github.com>
2023-05-02 00:51:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// const ham = document.querySelectorAll('popup');
+
+// function main(){
+    
+//     ham.classList.toggle('popup1')
+// } 
+
+// const pop =document.getElementsByClassName('popup');
+// function main(){
+//      console.log('h');
+ 
+// pop.classList.toggle('popup1')
+   
+ 
+//     }
+function dark(){
+const dark=document.querySelector('body').classList.toggle('dark')
+const qx=document.querySelector('.n2').classList.toggle('n2dark')
+const para=document.querySelector('.pera').classList.toggle('peradark')
+const icon=document.querySelector('.fa-solid').classList.toggle('fa-soliddark')
+const about=document.querySelector('.back-about').classList.toggle('.back-aboutdark')
+
+
+}
+function hamon(){
+     const pop =document.querySelector('.popup').classList.toggle('popup1')
+}
+
+function callme(){
+     alert('thank you for submission')
+}
